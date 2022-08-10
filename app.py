6ecfe359781e481adbf576d9b878bad8a0b2f595
impr.visible@gmail.com <impr.visible@gmail.com>
2022-08-11 00:33:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -458,6 +458,23 @@ def library():
     errorMessage = "Verify that the path is correct"
     routeToUse = "/getAllMovies"
     return render_template('allFilms.html', conditionIfOne=searchedFilmsUp0, errorMessage=errorMessage, routeToUse=routeToUse)
+
+#write a levenshtein distance function
+def levenshteinDistance(s1, s2):
+    if len(s1) < len(s2):
+        return levenshteinDistance(s2, s1)
+    if len(s2) == 0:
+        return len(s1)
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    return previous_row[-1]
     
 def jaroDistance(s1, s2):
     if len(s1) < len(s2):
