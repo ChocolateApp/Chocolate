9fcb2bd18f13662c212685b747048672b05b4b33
impr.visible@gmail.com <impr.visible@gmail.com>
2022-08-18 18:50:56 +0200

    patch a bug

--- static/js/settings.js
+++ static/js/settings.js
@@ -2,6 +2,12 @@ function saveSettings(event) {
     event.preventDefault()
     language = document.getElementById("language").value
     document.cookie = "language=" + language + "; path=/";
+    port = document.getElementById("port").value
+
+    if (port === "8000" || port === "8800") {
+        alert("The port " + port + " is reserved for the web server. Please choose another port.")
+        return
+    }
 
     form = document.getElementById("saveSettingsForm")
     form.action = "/saveSettings"
