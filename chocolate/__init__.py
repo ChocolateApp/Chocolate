ff5099aac0e1a064db841a3a60ac08fee8fa06e1
impr.visible@gmail.com <impr.visible@gmail.com>
2023-04-18 14:08:55 +0200

    First app.py separation
    Patched a bug where all static files can't be reached

--- chocolate/__init__.py
+++ chocolate/__init__.py
@@ -5,19 +5,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_login import LoginManager
 
-
 db = SQLAlchemy()
 loginManager = LoginManager()
 
 
 def create_app():
-    app = Flask(__name__)
+    dirPath = os.getcwd()
+    dirPath = os.path.dirname(__file__).replace("\\", "/")
+    dirPath = dirPath.replace("/chocolate", "")
+
+    app = Flask(__name__, static_folder=f"{dirPath}/static")
     app.secret_key = "ChocolateDBPassword"
 
     CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
-
-    dirPath = os.getcwd()
-    dirPath = os.path.dirname(__file__).replace("\\", "/")
     app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{dirPath}/database.db'
     app.config['MAX_CONTENT_LENGTH'] = 4096 * 4096
     app.config['UPLOAD_FOLDER'] = f"{dirPath}/static/img/"
