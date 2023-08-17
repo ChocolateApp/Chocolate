80afff9b7e7f43d1a3b83c976a4531377ece2831
impr.visible@gmail.com <impr.visible@gmail.com>
2023-08-17 16:01:34 +0200

    Fix: remove no necessary __init__ in tables

--- src/chocolate/tables.py
+++ src/chocolate/tables.py
@@ -46,24 +46,6 @@ class Movies(DB.Model):
     library_name = DB.Column(DB.String(255))
     alternatives_names = DB.Column(DB.Text)
 
-    def __init__(self, id, title, real_title, cover, banner, slug, description, note, date, genre, duration, cast, bande_annonce_url, adult, library_name, alternatives_names):
-        self.id = id
-        self.title = title
-        self.real_title = real_title
-        self.cover = cover
-        self.banner = banner
-        self.slug = slug
-        self.description = description
-        self.note = note
-        self.date = date
-        self.genre = genre
-        self.duration = duration
-        self.cast = cast
-        self.bande_annonce_url = bande_annonce_url
-        self.adult = adult
-        self.library_name = library_name
-        self.alternatives_names = alternatives_names
-
     def __repr__(self) -> str:
         return f"<Movies {self.title}>"
 
@@ -85,23 +67,6 @@ class Series(DB.Model):
     library_name = DB.Column(DB.String(255))
     adult = DB.Column(DB.String(255))
 
-    def __init__(self, id, name, original_name, genre, duration, description, cast, bande_annonce_url, serie_cover_path, banniere, note, date, serie_modified_time, adult, library_name):
-        self.id = id
-        self.name = name
-        self.original_name = original_name
-        self.genre = genre
-        self.duration = duration
-        self.description = description
-        self.cast = cast
-        self.bande_annonce_url = bande_annonce_url
-        self.serie_cover_path = serie_cover_path
-        self.banniere = banniere
-        self.note = note
-        self.date = date
-        self.serie_modified_time = serie_modified_time
-        self.library_name = library_name
-        self.adult = adult
-
     def __repr__(self) -> str:
         return f"<Series {self.name}>"
 
@@ -119,18 +84,6 @@ class Seasons(DB.Model):
     modified_date = DB.Column(DB.Float)
     number_of_episode_in_folder = DB.Column(DB.Integer)
 
-    def __init__(self, serie, release, episodes_number, season_number, season_id, season_name, season_description, season_cover_path, modified_date, number_of_episode_in_folder):
-        self.serie = serie
-        self.release = release
-        self.episodes_number = episodes_number
-        self.season_number = season_number
-        self.season_id = season_id
-        self.season_name = season_name
-        self.season_description = season_description
-        self.season_cover_path = season_cover_path
-        self.modified_date = modified_date
-        self.number_of_episode_in_folder = number_of_episode_in_folder
-
     def __repr__(self) -> str:
         return f"<Seasons {self.serie} {self.season_number}>"
 
@@ -146,18 +99,6 @@ class Episodes(DB.Model):
     slug = DB.Column(DB.String(255))
     intro_start = DB.Column(DB.Float)
     intro_end = DB.Column(DB.Float)
-
-    def __init__(self, season_id, episode_id, episode_name, episode_number, episode_description, episode_cover_path, release_date, slug, intro_start, intro_end):
-        self.season_id = season_id
-        self.episode_id = episode_id
-        self.episode_name = episode_name
-        self.episode_number = episode_number
-        self.episode_description = episode_description
-        self.episode_cover_path = episode_cover_path
-        self.release_date = release_date
-        self.slug = slug
-        self.intro_start = intro_start
-        self.intro_end = intro_end
 
     def __repr__(self) -> str:
         return f"<Episodes {self.season_id} {self.episodeNumber}>"
@@ -176,19 +117,6 @@ class Games(DB.Model):
     slug = DB.Column(DB.String(255))
     library_name = DB.Column(DB.String(255))
 
-    def __init__(self, console, id, title, real_title, cover, description, note, date, genre, slug, library_name):
-        self.console = console
-        self.id = id
-        self.title = title
-        self.real_title = real_title
-        self.cover = cover
-        self.description = description
-        self.note = note
-        self.date = date
-        self.genre = genre
-        self.slug = slug
-        self.library_name = library_name
-
     def __repr__(self) -> str:
         return f"<Games {self.title}>"
 
@@ -200,14 +128,6 @@ class OthersVideos(DB.Model):
     banner = DB.Column(DB.String(255))
     duration = DB.Column(DB.String(255))
     library_name = DB.Column(DB.String(255))
-
-    def __init__(self, video_hash, title, slug, banner, duration, library_name):
-        self.video_hash = video_hash
-        self.title = title
-        self.slug = slug
-        self.banner = banner
-        self.duration = duration
-        self.library_name = library_name
 
     def __repr__(self) -> str:
         return f"<OthersVideos {self.title}>"
@@ -363,9 +283,6 @@ class Playlist(DB.Model):
 class Language(DB.Model):
     language = DB.Column(DB.String(255), primary_key=True)
 
-    def __init__(self, language):
-        self.language = language
-
     def __repr__(self) -> str:
         return f"<Language {self.language}>"
 
@@ -379,15 +296,6 @@ class Actors(DB.Model):
     actor_birth_place = DB.Column(DB.String(255))
     actor_programs = DB.Column(DB.Text)
 
-    def __init__(self, name, actor_id, actor_image, actor_description, actor_birth_date, actor_birth_place, actor_programs):
-        self.name = name
-        self.actor_id = actor_id
-        self.actor_image = actor_image
-        self.actor_description = actor_description
-        self.actor_birth_date = actor_birth_date
-        self.actor_birth_place = actor_birth_place
-        self.actor_programs = actor_programs
-
     def __repr__(self) -> str:
         return f"<Actors {self.name}>"
 
@@ -399,13 +307,6 @@ class Libraries(DB.Model):
     lib_folder = DB.Column(DB.Text)
     available_for = DB.Column(DB.JSON)
 
-    def __init__(self, lib_name, lib_image, lib_type, lib_folder, available_for):
-        self.lib_name = lib_name
-        self.lib_image = lib_image
-        self.lib_type = lib_type
-        self.lib_folder = lib_folder
-        self.available_for = available_for
-
     def __repr__(self) -> str:
         return f"<Libraries {self.lib_name}>"
 
@@ -414,11 +315,6 @@ class MusicPlayed(DB.Model):
     user_id = DB.Column(DB.Integer, primary_key=True)
     music_id = DB.Column(DB.Integer, primary_key=True)
     play_count = DB.Column(DB.Integer)
-
-    def __init__(self, user_id, music_id, play_count):
-        self.user_id = user_id
-        self.music_id = music_id
-        self.play_count = play_count
 
     def __repr__(self) -> str:
         return f"<MusicPlayed {self.user_id}>"
@@ -430,12 +326,6 @@ class MusicLiked(DB.Model):
     liked = DB.Column(DB.Integer)
     liked_at = DB.Column(DB.Integer, default=int(time()))
 
-    def __init__(self, user_id, music_id, liked, liked_at):
-        self.user_id = user_id
-        self.music_id = music_id
-        self.liked = liked
-        self.liked_at = int(time())
-
     def __repr__(self) -> str:
         return f"<MusicLiked {self.user_id}>"
 
@@ -443,11 +333,6 @@ class LatestEpisodeWatched(DB.Model):
     user_id = DB.Column(DB.Integer, primary_key=True)
     serie_id = DB.Column(DB.Integer, primary_key=True)
     episode_id = DB.Column(DB.Integer)
-
-    def __init__(self, user_id, serie_id, episode_id):
-        self.user_id = user_id
-        self.serie_id = serie_id
-        self.episode_id = episode_id
     
     def __repr__(self) -> str:
         return f"<LatestEpisodeWatched {self.user_id}>"
@@ -457,3 +342,10 @@ class InviteCodes(DB.Model):
 
     def __repr__(self) -> str:
         return f"<InviteCode {self.code}>"
+    
+class LibrariesMerge(DB.Model):
+    parent_lib = DB.Column(DB.String(255), primary_key=True)
+    child_lib = DB.Column(DB.String(255), primary_key=True)
+
+    def __repr__(self) -> str:
+        return f"<LibrariesMerge {self.parent_lib}>"
