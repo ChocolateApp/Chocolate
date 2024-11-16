0b1273fba807b4ac3729d16628089d72183eb03d
Impre-visible <impr.visible@gmail.com>
2024-11-16 18:08:20 +0100

    ♻️ feat: Register settings blueprint in API routes

--- src/chocolate_app/routes/api/index.py
+++ src/chocolate_app/routes/api/index.py
@@ -3,9 +3,11 @@ from flask import Blueprint
 from chocolate_app.routes.api.auth import auth_bp
 from chocolate_app.routes.api.medias import medias_bp
 from chocolate_app.routes.api.watch import watch_bp
+from chocolate_app.routes.api.settings import settings_bp
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 api_bp.register_blueprint(auth_bp)
 api_bp.register_blueprint(medias_bp)
 api_bp.register_blueprint(watch_bp)
+api_bp.register_blueprint(settings_bp)
