b0ad35dfd6e527c6f21eedb63a45b29f26f93bc8
Impre-visible <impr.visible@gmail.com>
2024-11-16 18:44:54 +0100

    ♻️ feat: Implement accounts settings management with CRUD operations in API

--- src/chocolate_app/routes/api/settings.py
+++ src/chocolate_app/routes/api/settings.py
@@ -2,9 +2,10 @@ import os
 
 from iso639 import Lang
 
-from chocolate_app import CONFIG_PATH, get_config, get_dir_path, write_config
+from chocolate_app import CONFIG_PATH, DB, get_config, get_dir_path, write_config
 from chocolate_app.routes.api.auth import token_required
 from flask import Blueprint, request, Response
+from chocolate_app.tables import Users
 from chocolate_app.utils.utils import generate_response, Codes
 
 
@@ -29,7 +30,6 @@ def clean_json_for_config(json: dict) -> dict:
 def handle_general_settings(method) -> Response:
     def get() -> Response:
         config = get_config()
-        print(config.__dict__)
         return generate_response(Codes.SUCCESS, data=config.__dict__["_sections"])
 
     def post() -> Response:
@@ -52,18 +52,109 @@ def handle_general_settings(method) -> Response:
         return generate_response(Codes.INVALID_METHOD)
 
 
-@settings_bp.route("/<section>", methods=["GET", "POST"])
+def handle_accounts_settings(method) -> Response:
+    def get() -> Response:
+        accounts = Users.query.all()
+        accounts_list = []
+        for account in accounts:
+            accounts_list.append(
+                {
+                    "id": account.id,
+                    "name": account.name,
+                    "account_type": account.account_type,
+                }
+            )
+
+        return generate_response(Codes.SUCCESS, data=accounts_list)
+
+    def post() -> Response:
+        if not request.json:
+            return generate_response(Codes.INVALID_MEDIA_TYPE)
+
+        data = request.json
+
+        if "id" not in data:
+            return generate_response(Codes.MISSING_DATA, True)
+
+        account = Users.query.filter_by(id=data["id"]).first()
+
+        if not account:
+            return generate_response(Codes.USER_NOT_FOUND, True)
+
+        if "name" in data:
+            account.name = data["name"]
+
+        if "account_type" in data:
+            account.account_type = data["account_type"]
+
+        DB.session.commit()
+
+        return generate_response(Codes.SUCCESS)
+
+    def delete() -> Response:
+        if not request.json:
+            return generate_response(Codes.INVALID_MEDIA_TYPE)
+
+        data = request.json
+
+        if "id" not in data:
+            return generate_response(Codes.MISSING_DATA, True)
+
+        account = Users.query.filter_by(id=data["id"]).first()
+
+        if not account:
+            return generate_response(Codes.USER_NOT_FOUND, True)
+
+        DB.session.delete(account)
+        DB.session.commit()
+
+        return generate_response(Codes.SUCCESS)
+
+    def put() -> Response:
+        if not request.json:
+            return generate_response(Codes.INVALID_MEDIA_TYPE)
+
+        data = request.json
+
+        if "name" not in data or "password" not in data or "account_type" not in data:
+            return generate_response(Codes.MISSING_DATA, True)
+
+        user = Users(
+            name=data["name"],
+            password=data["password"],
+            account_type=data["account_type"],
+            profile_picture="static/images/default_profile_picture.jpg",
+        )
+        DB.session.add(user)
+        DB.session.commit()
+
+        return generate_response(Codes.SUCCESS)
+
+    if method == "GET":
+        return get()
+    elif method == "POST":
+        return post()
+    elif method == "DELETE":
+        return delete()
+    elif method == "PUT":
+        return put()
+    else:
+        return generate_response(Codes.INVALID_METHOD)
+
+
+@settings_bp.route("/<section>", methods=["GET", "POST", "PUT", "DELETE"])
 @token_required
 def settings(section) -> Response:
     if section == "general":
         return handle_general_settings(request.method)
+    elif section == "accounts":
+        return handle_accounts_settings(request.method)
     else:
         return generate_response(Codes.MISSING_DATA, True)
 
 
 @settings_bp.route("/languages", methods=["GET"])
 def languages() -> Response:
-    # get all the jsons in the static/lang folder (the file.json is the iso code)
     languages = []
     dir_path = get_dir_path()
     for file in os.listdir(f"{dir_path}/static/lang"):
