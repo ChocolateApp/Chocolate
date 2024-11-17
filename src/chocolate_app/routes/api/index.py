83f00d6511aa8643eeb5e357e56e52cbf4a06fc4
Impre-visible <impr.visible@gmail.com>
2024-11-17 16:11:41 +0100

    ✨ feat: Register profil blueprint in API routes

--- src/chocolate_app/routes/api/index.py
+++ src/chocolate_app/routes/api/index.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
 from chocolate_app.routes.api.auth import auth_bp
+from chocolate_app.routes.api.profil import profil_bp
 from chocolate_app.routes.api.medias import medias_bp
 from chocolate_app.routes.api.watch import watch_bp
 from chocolate_app.routes.api.settings import settings_bp
@@ -11,3 +12,4 @@ api_bp.register_blueprint(auth_bp)
 api_bp.register_blueprint(medias_bp)
 api_bp.register_blueprint(watch_bp)
 api_bp.register_blueprint(settings_bp)
+api_bp.register_blueprint(profil_bp)
